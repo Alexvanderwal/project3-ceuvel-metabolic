8b721e72677f494f5805e416d1da51e089be2c67
Cascuna <amvvanderwal@live.nl>
2018-05-09 12:11:40 +0200

    Added animation package for barba

--- app/static/js/site.js
+++ app/static/js/site.js
@@ -1,1 +1,21 @@
+// let HideShowTransition = Barba.BaseTransition.extend({
+//   start: function() {
+//     this.newContainerLoading.then(this.finish.bind(this));
+//     console.log("works");
+//   },
+
+//   finish: function() {
+//     document.body.scrollTop = 0;
+//     this.done();
+//   }
+// });
+
+// Barba.Pjax.getTransition = function() {
+//   return HideShowTransition;
+// };
+
+function navigate(event) {
+  console.log(event.target);
+}
+
 Barba.Pjax.start();
