6984af3b8d272f9f6511d736d83b657508ad57be
Cascuna <amvvanderwal@live.nl>
2018-05-09 14:24:46 +0200

    Page change animation changed and tweaked

--- app/static/js/site.js
+++ app/static/js/site.js
@@ -15,18 +15,29 @@
 // };
 
 function navigate(href) {
-  console.log(href);
-  let hexagonSvg = href.childNodes[1];
-  //   hexagonSvg.setAttribute("data-transition", "scaleUp");
-  href.setAttribute("data-transition", "scaleUp");
-  console.log(hexagonSvg);
-  hexagonSvg.querySelector(".svgText").style.opacity = 0;
+  //   console.log(href);
+  let links = document.querySelectorAll(".readmore");
+  console.log(links);
+  for (link of links) {
+    console.log(link);
+    if (link !== href) {
+      link.style.background = "rgba(0,0,0,0.50)";
+    }
+    link.querySelector("svg").style.opacity = 0;
+  }
+  let shipBody = document.querySelector(".shipBody");
+  shipBody.style.zIndex = 6;
+  //   let hexagonSvg = href.childNodes[1];
+  //   //   hexagonSvg.setAttribute("data-transition", "scaleUp");
+  //   href.setAttribute("data-transition", "scaleUp");
+  //   console.log(hexagonSvg);
+  //   hexagonSvg.querySelector(".svgText").style.opacity = 0;
 
-  href.classList.toggle("scalingSvg");
-  //   hexagonSvg.classList.toggle("scalingSvg");
+  //   href.classList.toggle("scalingSvg");
+  //   //   hexagonSvg.classList.toggle("scalingSvg");
 
-  console.log(hexagonSvg.classList);
-  console.log(href.childNodes[1]);
+  //   console.log(hexagonSvg.classList);
+  //   console.log(href.childNodes[1]);
 }
-Barba.transitionLength = 600;
+Barba.transitionLength = 1000;
 Barba.Pjax.start();
