be3278a9d390e762dc8f20939508c572ce06901f
Cascuna <amvvanderwal@live.nl>
2018-05-09 13:25:30 +0200

    Added more transition magic This will improve the immersion, and create the illusion that all pages are on the site

--- app/static/js/site.js
+++ app/static/js/site.js
@@ -14,8 +14,19 @@
 //   return HideShowTransition;
 // };
 
-function navigate(event) {
-  console.log(event.target);
-}
+function navigate(href) {
+  console.log(href);
+  let hexagonSvg = href.childNodes[1];
+  //   hexagonSvg.setAttribute("data-transition", "scaleUp");
+  href.setAttribute("data-transition", "scaleUp");
+  console.log(hexagonSvg);
+  hexagonSvg.querySelector(".svgText").style.opacity = 0;
 
+  href.classList.toggle("scalingSvg");
+  //   hexagonSvg.classList.toggle("scalingSvg");
+
+  console.log(hexagonSvg.classList);
+  console.log(href.childNodes[1]);
+}
+Barba.transitionLength = 600;
 Barba.Pjax.start();
