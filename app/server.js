a44a9d5cbb9af79a4d356b2c4cbf70233630c1ba
Cascuna <amvvanderwal@live.nl>
2018-05-14 11:38:38 +0200

    Added a basic implementation for AMQP sockets

--- app/server.js
+++ app/server.js
@@ -6,6 +6,72 @@ const io = require("socket.io")(http);
 var router = express.Router();
 var fish = require("./fish/");
 var plants = require("./plants/");
+var amqp = require("amqplib");
+var ampqTest = require("amqp");
+// amqp://consumer:zHJR6WPpgUDLt5cF@rabbit.spectral.energy/
+// var connection = amqpTest.createConnection(
+//   {
+//     host: "rabbit.spectral.energy",
+//     login: "consumer",
+//     password: "zHJR6WPpgUDLt5cF@r",
+//     vhost: "/"
+//   },
+//   { defaultExchangeName: "aquaponics" }
+// );
+
+// connection.on("ready", function() {
+//   connection.exchange("aquaponics");
+// });
+
+amqp
+  .connect("amqp://consumer:zHJR6WPpgUDLt5cF@rabbit.spectral.energy/")
+  .then(function(conn) {
+    process.once("SIGINT", function() {
+      conn.close();
+    });
+    return conn.createChannel().then(function(channel) {
+      channel
+        .assertExchange("aquaponics", "topic", { durable: true })
+        .then(result => {
+          var queue = channel.assertQueue("", { exclusive: true });
+          queue = queue.then(function(_queue) {
+            channel.bindQueue(_queue.queue, "aquaponics", "deceuvel");
+            console.log(_queue);
+            return channel.consume(
+              _queue.queue,
+              function(msg) {
+                console.log(" [x] Received '%s'", msg.content.toString());
+              },
+              { noAck: true }
+            );
+          });
+          return queue.then(function(_consumeOk) {
+            console.log(" [*] Waiting for messages. To exit press CTRL+C");
+          });
+        });
+    });
+  });
+
+// // add this for better debuging
+// connection.on("error", function(e) {
+//   console.log("Error from amqp: ", e);
+// });
+
+// // Wait for connection to become established.
+// connection.on("ready", function() {
+//   // Use the default 'amq.topic' exchange
+//   connection.queue("my-queue", function(q) {
+//     // Catch all messages
+//     q.bind("#");
+
+//     // Receive messages
+//     q.subscribe(function(message) {
+//       // Print messages to stdout
+//       console.log(message);
+//     });
+//   });
+// });
+
 //0.0.0.0 instead of 127.0.0.1 forces the server to redirect to the local endpoints instead of the exposed local endpoints
 
 app
